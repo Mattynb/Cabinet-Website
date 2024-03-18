39cfa71c5df691b57295601c5c4e1f902bf12966
HKES-04 <husseinkhawalelsultan@gmail.com>
2024-03-18 18:31:14 -0400

    Created a Shop page to include the cabinet list items

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,37 @@
-import {useAuth} from './contexts/AuthContext'
-import Header from './components/Header'
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { useAuth } from "./contexts/AuthContext";
+import Header from "./components/Header";
+import Shop from "./components/ShopPage/Shop";
 
 export default function App() {
-  const {isLoggedIn} = useAuth()
+  const { isLoggedIn } = useAuth();
 
   return (
-    <div className='App'>
-      <Header />
-
-      {isLoggedIn ? <LoggedInText /> : <LoggedOutText />}
-    </div>
-  )
+    <Router>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route path="/shop" element={<Shop />} />
+          <Route
+            path="/"
+            element={isLoggedIn ? <LoggedInText /> : <LoggedOutText />}
+          />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
 const LoggedInText = () => {
-  const {account} = useAuth()
+  const { account } = useAuth();
 
-  return <p>Hey, {account.username}! I'm happy to let you know: you are authenticated!</p>
-}
+  return (
+    <p>
+      Hey, {account.username}! I'm happy to let you know: you are authenticated!
+    </p>
+  );
+};
 
 const LoggedOutText = () => (
   <p>Don't forget to start your backend server, then authenticate yourself.</p>
-)
+);
