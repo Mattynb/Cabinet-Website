1973deb6d8760d9fa9ba7ec88c73bb5a420a14c5
KevinXT <Kevin.Tran007@umb.edu>
2024-03-18 12:07:13 -0400

    Fixed server/constant/index.js

--- server/constants/index.js
+++ server/constants/index.js
@@ -2,10 +2,10 @@ const ORIGIN = '*'
 const PORT = process.env.PORT || 8080
 
 // for "atlas" edit MONGO_URI in -> .env file || for "community server" edit <MyDatabase>
-const MONGO_URI = process.env.MONGO_URI || ""
+const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/MyDatabase'
 const MONGO_OPTIONS = {}
 
-const JWT_SECRET = process.env.JWT_SECRET || ""
+const JWT_SECRET = process.env.JWT_SECRET || 'unsafe_secret'
 
 module.exports = {
   ORIGIN,
