4a46c6c173847419cb8e4f4153c34c6fa30c9462
Tsegereda <Koki@Ts-MacBook-Pro.local>
2024-03-28 21:08:26 -0400

    Updated the app.js,added  styling, and JSX component for design inspiration

--- client/src/App.js
+++ client/src/App.js
@@ -1,38 +1,42 @@
+
 import React from 'react';
 import Header from './components/Header';
 import ContactForm from './components/home/ContactSection';
 import { useAuth } from './contexts/AuthContext';
 import DesignInspiration from './components/home/DesignInspiration';
-import { BrowserRouter as Router} from 'react-router-dom';
-//import DesignInspirationSection from './components/home/DesignInspirationSection'; // Adjust the path as necessary
-//import GalleryPage from './components/GalleryPage'; // Placeholder for  gallery component
+import { BrowserRouter as Router } from 'react-router-dom';
+
+import 'swiper/swiper-bundle.min.css';
+
+// ... Your other import statements
 
 export default function App() {
-  const { isLoggedIn } = useAuth()
+  const { isLoggedIn } = useAuth();
 
   return (
-    <Router> 
-    <div className='App'>
-      <Header />
+    <Router>
+      <div className='App'>
+        <Header />
 
-      {isLoggedIn ? <LoggedInText /> : <LoggedOutText />}
-      {/* Render the DesignInspirationSection component */}
-      <DesignInspiration/>
-      {/* <GalleryPage /> */}
-      <ContactForm />
-      
-    </div>
+        {isLoggedIn ? <LoggedInText /> : <LoggedOutText />}
+        
+        <DesignInspiration/>
+        <ContactForm />
+      </div>
     </Router>
-  )
+  );
 }
+
+
 
 const LoggedInText = () => {
-  const { account } = useAuth()
+  const { account } = useAuth();
 
-  return <p>Hey, {account.username}! I'm happy to let you know: you are authenticated!</p>
-}
+  return <p>Hey, {account.username}! I'm happy to let you know: you are authenticated!</p>;
+};
 
 const LoggedOutText = () => (
   <p>Don't forget to start your backend server, then authenticate yourself.</p>
-)
+);
+
 
--- client/src/components/home/DesignInspiration.jsx
+++ client/src/components/home/DesignInspiration.jsx
@@ -1,11 +1,17 @@
 
+
 import React from 'react';
-import { useNavigate } from 'react-router-dom'; // Import useNavigate instead of useHistory
-import './DesignInspiration.css'; // Your custom styles
-import { Link } from 'react-router-dom';
+import 'swiper/swiper.min.css'; // core Swiper
+import 'swiper/modules/pagination/pagination.min.css'; // Pagination module
+import 'swiper/modules/navigation/navigation.min.css'; // Navigation module
+import { useNavigate, Link } from 'react-router-dom';
+import './DesignInspiration.css';
+import { Swiper, SwiperSlide } from 'swiper/react/swiper-react';
+import { Pagination, Navigation } from 'swiper';
+
 
 const DesignInspiration = () => {
-  const navigate = useNavigate(); // useNavigate hook to get navigate function
+   const navigate = useNavigate();
 
   return (
     <div className="design-inspiration">
@@ -13,23 +19,35 @@ const DesignInspiration = () => {
         <h2 className="section-title">50+ Beautiful Design Inspiration</h2>
         <p className="section-description">Our designer already made a lot of beautiful prototypes of rooms that inspire you</p>
       </div>
-      <div className="image-gallery">
-        <Link to="/page-for-imageL">
-          <img src="/ImageL.jpg" className="imageL" alt="Inspiration Left" />
-        </Link>
-        <Link to="/page-for-TGW-OSCC">
-          <img src="/TGW-OSCC.jpg" className="TGW-OSCC" alt="Inspiration Middle" />
-        </Link>
-        <Link to="/page-for-imageR">
-          <img src="/ImageR.png" className="imageR" alt="Inspiration Right" />
-        </Link>
-      </div>
+      <Swiper
+        modules={[Pagination, Navigation]}
+        spaceBetween={50}
+        slidesPerView={1}
+        pagination={{ clickable: true }}
+        navigation={true}
+        className="swiper-container"
+        >
+          <SwiperSlide>
+          <Link to="/page-for-imageL">
+            <img className='swiper-item' src="/ImageL.jpg" alt="Inspiration Left" />
+          </Link>
+        </SwiperSlide>
+        <SwiperSlide>
+          <Link to="/page-for-TGW-OSCC">
+            <img className='swiper-item' src="/TGW-OSCC.jpg" alt="Inspiration Middle" />
+          </Link>
+        </SwiperSlide>
+        <SwiperSlide>
+          <Link to="/page-for-imageR">
+            <img className='swiper-item' src="/ImageR.png" alt="Inspiration Right" />
+          </Link>
+        </SwiperSlide>
+       
+       </Swiper>
+        
       <button className="explore-button" onClick={() => navigate('/gallery')}>Gallery</button>
     </div>
   );
 };
-
 export default DesignInspiration;
-
-
 
