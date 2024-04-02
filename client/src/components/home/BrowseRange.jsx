60c97936d1c9954f6b5d7fe648f500d6b636f8a9
Waliyat1 <53194072+Waliyat1@users.noreply.github.com>
2024-04-02 13:16:30 -0400

    Browse the range all set

--- client/src/components/home/BrowseRange.jsx
+++ client/src/components/home/BrowseRange.jsx
@@ -6,25 +6,23 @@ const BrowseRange = () => {
   return (
     <section className="browse-range">
       <div className="browse-title">
-        <h1>  Browse The Range </h1> 
+        <h1>Browse The Range</h1> 
       </div>
       <div className="image-container">
-        <Link to="/image/1">
-          <img src="collections-image/SW Kitchen_Small 1.png" alt="Image 1" />
-          <div className="title-bar">Framed</div>
-        </Link>
-      </div>
-      <div className="image-container">
-        <Link to="/image/3">
-          <img src="collections-image/SW Kitchen_Small 1.png" alt="Image 3" />
-          <div className="title-bar">Frameless</div>
-        </Link>
-      </div>
-      <div className="image-container">
-        <Link to="/image/3">
-          <img src="collections-image/SW Kitchen_Small 1.png" alt="Image 3" />
-          <div className="title-bar">Universal Design</div>
-        </Link>
+        <div className="image-wrapper">
+          <Link to="/image/1">
+          <img className="imageL" src="collections-image/ImageL.png" alt="Image 1" />
+            <div className="title-bar">Framed</div>
+          </Link>
+          <Link to="/image/2">
+            <img className="imageM" src="collections-image/FramelessCollec.png" alt="Image 2" />
+            <div className="title-bar">Frameless</div>
+          </Link>
+          <Link to="/image/3">
+            <img className="imageR" src="collections-image/kitchenC2.png" alt="Image 3" />
+            <div className="title-bar">Universal Design</div>
+          </Link>
+        </div>
       </div>
     </section>
   );
