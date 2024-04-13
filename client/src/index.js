e0ef0a21f170870487f1d642fdfd649873704fe5
XiaoyanLin02 <144283232+XiaoyanLin02@users.noreply.github.com>
2024-04-13 12:37:58 -0400

    burger list, footer subscriber, barebone checkout page (#33)
    * Navbar responsive
    
    * Burger list, footer subcriber, and barebone checkout page
    
    * removing unused imports
    
    * Refactor Header component to import OnlineIndicator
    
    ---------
    
    Co-authored-by: Xiaoyan Lin <xiaoyanlin@Xiaoyans-MBP.hsd1.ma.comcast.net>
    Co-authored-by: Matheus <matheus.berbet001@umb.edu>
    Co-authored-by: Xiaoyan Lin <xiaoyanlin@xiaoyans-mbp.ssw.umb.edu>

--- client/src/index.js
+++ client/src/index.js
@@ -6,12 +6,11 @@ import CssBaseline from '@mui/material/CssBaseline'
 import '@fontsource/roboto'
 import './styles/index.css'
 
-ReactDOM.render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <AuthProvider>
       <CssBaseline />
       <App />
     </AuthProvider>
-  </React.StrictMode>,
-  document.getElementById('root'),
+  </React.StrictMode>
 )
