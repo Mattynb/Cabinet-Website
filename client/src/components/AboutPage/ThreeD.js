ce4c78911727fbde8ac740c1f41a59622196fcb8
XiaoyanLin02 <144283232+XiaoyanLin02@users.noreply.github.com>
2024-04-16 18:46:45 -0400

    Update OrbitControl

--- client/src/components/AboutPage/ThreeD.js
+++ client/src/components/AboutPage/ThreeD.js
@@ -1,7 +1,7 @@
 import React, { useRef, useEffect } from 'react';
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
-import { OrbitControls } from 'three/addons/controls/OrbitControls.js';  
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';  
 
 const ThreeD = () => {
   const sceneRef = useRef();
