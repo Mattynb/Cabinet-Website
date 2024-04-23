a93032c7ca0845478b5542ece59ce2a968b7c836
Xiaoyan Lin <xiaoyanlin@Xiaoyans-MBP.hsd1.ma.comcast.net>
2024-04-22 22:43:28 -0400

    specify import from three

--- client/src/components/AboutPage/ThreeD.js
+++ client/src/components/AboutPage/ThreeD.js
@@ -1,5 +1,5 @@
 import React, { useRef, useEffect } from 'react';
-import * as THREE from 'three';
+import { Scene, PerspectiveCamera, WebGLRenderer, AmbientLight, DirectionalLight } from 'three';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';  
 import style from "../../styles/AboutPage/About.module.css";
@@ -12,30 +12,30 @@ const ThreeD = () => {
 
     window.onload = function() {
       // Set up scene
-      scene = new THREE.Scene();
+      scene = new Scene();
 
       // setup the camera
       var fov = 75;
       var ratio =  309 / 523; // figma layout
       var zNear = 0.1;
       var zFar = 1000;
-      camera = new THREE.PerspectiveCamera(fov, ratio, zNear, zFar);
+      camera = new PerspectiveCamera(fov, ratio, zNear, zFar);
       camera.position.set(5, 10, 20);
 
-      renderer = new THREE.WebGLRenderer({antialias: true},{alpha: true});
+      renderer = new WebGLRenderer({antialias: true},{alpha: true});
       renderer.setClearColor(0x000000, 0);
       renderer.setSize(309, 523);
       sceneRef.current.appendChild(renderer.domElement);
 
       // set up ambient light
-      var ambientLight = new THREE.AmbientLight();
+      var ambientLight = new AmbientLight();
       scene.add( ambientLight );
 
       // set up directional light
-      var light = new THREE.DirectionalLight( 0xffffff, 10.0 );
+      var light = new DirectionalLight( 0xffffff, 10.0 );
       light.position.set( -50, 50, 50);
       scene.add( light );
-      var light2 = new THREE.DirectionalLight( 0xffffff, 10.0 );
+      var light2 = new DirectionalLight( 0xffffff, 10.0 );
       light2.position.set( 50, -50, -50);
       scene.add( light2 );
 
