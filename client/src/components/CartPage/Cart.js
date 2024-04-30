742fcb8b4be4182c858c93245347d2a52790a253
KevinXT <47616621+KevinXT@users.noreply.github.com>
2024-04-30 11:54:43 -0500

    Home browserange title bar fix (#59)
    * Cabinet.js and cabinetRoute.js for GET /api/products
    
    * Created Cart and CarProductCard
    
    * Created Cart and CarProductCard Fixed Constant Index.js
    
    * fix
    
    * Responsiveness for cart-page
    
    * Responsiveness for cart-page
    
    * Fixed the css for the browse section
    
    * Empty commit
    
    * Empty commit
    
    * changed import { Checkout } from './components/CheckoutPage/Checkout'
    
    * changed import Checkout from './components/CheckoutPage/Checkout'
    
    * Merging Main
    
    ---------
    
    Co-authored-by: Matheus Berbet <matheus.berbet001@umb.edu>

--- client/src/components/CartPage/Cart.js
+++ client/src/components/CartPage/Cart.js
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from "react";
 import "../../styles/CartPage/Cart.css";
 import CartProductCard from './CartProductCard';
 import { Link } from 'react-router-dom';
-import Banner from "../Banner";
 
 function Cart() {
   // State to manage the items in the shopping cart
@@ -50,96 +49,47 @@ function Cart() {
     const total = cartItems.reduce((total, item) => total + item.price * item.quantity, 0);
     return total.toFixed(2); // Limit to two decimal places
   };
-
-  useEffect(() => {
-    // Mock product object for testing
-    const testProduct = {
-      id: 1,
-      name: "Test Product",
-      price: 10.99,
-      quantity: 1, // Initial quantity
-      // Other properties if needed
-    };
-
-    const testProduct2 = {
-      id: 2,
-      name: "Test Product2",
-      price: 8.99,
-      quantity: 5, // Initial quantity
-      // Other properties if needed
-    };
-
-    const testProduct3 = {
-      id: 3,
-      name: "Test Product3",
-      price: 15.99,
-      quantity: 8, // Initial quantity
-      // Other properties if needed
-    };
-
-    const testProduct4 = {
-      id: 4,
-      name: "Test Product4",
-      price: 15.99,
-      quantity: 6, // Initial quantity
-      // Other properties if needed
-    };
-
-    // Add the test product to the cart
-    addToCart(testProduct);
-    addToCart(testProduct2);
-    addToCart(testProduct3);
-    addToCart(testProduct4);
-  }, []);
-
+  
   return (
     <section className="shop">
-      <div className="shop-hero">
-        <img src="/shop-hero.jpg" alt="Shop Hero" />
-        <div className="shop-hero-title">Cart</div>
-        <div className="shop-hero-subtitle">
-          <span className="shop-hero-home">Home &gt;</span>{" "}
-          <span className="shop-hero-cart">Cart</span>
-        </div>
-      </div>
-      <div className="checkout-cart-container">
-        <div className="cart-total">
-          <div className="cart-totals-title">Cart Totals</div>
-          <div className="subtotal-label">Subtotal</div>
-          <div className="subtotal-value">${calculateTotal()}</div>
-          <div className="total-label">Total</div>
-          <div className="total-value">${calculateTotal()}</div>
-          <Link to={{
-                    pathname: "/checkout",
-                    state: { cartItems: cartItems,
-                    totalPrice: calculateTotal() }  // Pass cartItems to the checkout page
-                }}  className="checkout-button">Checkout</Link>
-        </div>
-        <div className="cart-header-container">
-          <div className="cart-header">
-            <span className="cart-header-text product-text">Product</span>
-            <span className="cart-header-text price-text">Price</span>
-            <span className="cart-header-text quantity-text">Quantity</span>
-            <span className="cart-header-text subtotal-text">Subtotal</span>
-          </div>
-          <div className="cart-container">
-            <div className="cart-scrollable">
-              {/* Render CartProductCard components for each item in the cart */}
-              {cartItems.map(item => (
-                <CartProductCard 
-                  key={item.id} 
-                  cabinet={item} 
-                  onDelete={handleDelete} 
-                  onQuantityChange={handleQuantityChange} 
-                />
-              ))}
-            </div>
-          </div>
-        </div>
-      </div>
-      <Banner />
-    </section>
-    
+  <div className="shop-hero">
+    <img src="/shop-hero.jpg" alt="Shop Hero" />
+    <div className="shop-hero-title">Cart</div>
+    <div className="shop-hero-subtitle">
+      <span className="shop-hero-home">Home &gt;</span>{" "}
+      <span className="shop-hero-cart">Cart</span>
+    </div>
+  </div>
+  <div className="cart-header-container">
+    <div className="cart-header">
+      <span className="cart-header-text product-text">Product</span>
+      <span className="cart-header-text price-text">Price</span>
+      <span className="cart-header-text quantity-text">Quantity</span>
+      <span className="cart-header-text subtotal-text">Subtotal</span>
+    </div>
+  </div>
+  <div className="cart-container">
+    <div className="cart-scrollable">
+      {/* Render CartProductCard components for each item in the cart */}
+      {cartItems.map(item => (
+        <CartProductCard 
+          key={item.id} 
+          cabinet={item} 
+          onDelete={handleDelete} 
+          onQuantityChange={handleQuantityChange} 
+        />
+      ))}
+    </div>
+  </div>
+    <div className="cart-total">
+      <div className="cart-totals-title">Cart Totals</div>
+      <div className="subtotal-label">Subtotal</div>
+      <div className="subtotal-value">${calculateTotal()}</div>
+      <div className="total-label">Total</div>
+      <div className="total-value">${calculateTotal()}</div>
+      <Link to="/checkout" className="checkout-button">Checkout</Link>
+    </div>
+</section>
   );
 
 }
--- client/src/components/home/BrowseRange.jsx
+++ client/src/components/home/BrowseRange.jsx
@@ -10,15 +10,15 @@ const BrowseRange = () => {
       </div>
       <div className="image-container">
         <div className="image-wrapper">
-          <Link to="../shop">
+          <Link to="../shop" className="link-bar">
           <img className="imageL" src="https://media.discordapp.net/attachments/1215179484110790706/1226253331023003790/ImageL.jpg?ex=66241868&is=6611a368&hm=f9b040ffad011854fe5216b19f5cbf12bb663176410d351ddbbafc861a07b010&=&format=webp&width=936&height=702" alt="" />
             <div className="title-bar">Framed</div>
           </Link>
-          <Link to="../shop">
+          <Link to="../shop" className="link-bar">
             <img className="imageM" src="https://media.discordapp.net/attachments/1215179484110790706/1226253330083221547/TGW-OSCC.jpg?ex=66241868&is=6611a368&hm=6367f7eb96a3a14d069106184be3a6f9e70f65d31def58740430e6c38fe8875c&=&format=webp&width=1248&height=702" alt="" />
             <div className="title-bar">Frameless</div>
           </Link>
-          <Link to="../shop">
+          <Link to="../shop" className="link-bar">
             <img className="imageR" src="https://media.discordapp.net/attachments/1073422681808048231/1227323910966476800/kitchenC2.png?ex=6627fd76&is=66158876&hm=4ef65d9ecf13672d28f551788f3d5ee5bae3ebb09e17f3107d764a6d61abb1d4&=&format=webp&quality=lossless&width=1410&height=822" alt="" />
             <div className="title-bar">Universal Design</div>
           </Link>
